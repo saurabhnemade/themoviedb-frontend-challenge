4918f246a0bced3419480386dcc048e1601be7f1
Saurabh Nemade <saurabhnemade@users.noreply.github.com>
2018-12-02 14:29:33 +0530

    Adding the semanitc ui for css and basic components

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import './App.css';
 import HomeContainer from './pages/Home/HomeContainer';
 import NotFound from './pages/404/NotFound';
 
+//Semantic UI CSS Setup
+import 'semantic-ui-css/semantic.min.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Switch>
               <Route exact path='/' component={HomeContainer}/>
-              {/* both /roster and /roster/:number begin with /roster */}
               <Route path='/roster' component={HomeContainer}/>
               <Route path='/schedule' component={HomeContainer}/>
               <Route component={NotFound} />
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
+
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 Home page
+                <Button>Click Here</Button>
             </div>
         );
     }
