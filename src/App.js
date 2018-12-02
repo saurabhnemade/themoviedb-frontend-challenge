5736ba9eb3a5028c3d436322962b5b3285c37280
Saurabh Nemade <saurabhnemade@users.noreply.github.com>
2018-12-02 14:48:24 +0530

    Added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Link} from 'react-router-dom';
+import {Menu, Input, Grid} from 'semantic-ui-react';
 import './App.css';
 
 /** Import pages here **/
@@ -13,12 +14,34 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Switch>
-              <Route exact path='/' component={HomeContainer}/>
-              <Route path='/roster' component={HomeContainer}/>
-              <Route path='/schedule' component={HomeContainer}/>
-              <Route component={NotFound} />
-          </Switch>
+          <Grid>
+              <Grid.Column width={8}>
+                  <Menu vertical>
+                      <Menu.Item name='Home' as={Link} to='/home'>
+                          Home
+                      </Menu.Item>
+                      <Menu.Item name='Trending' as={Link} to='/trending'>
+                          Trending
+                      </Menu.Item>
+                      <Menu.Item name='inbox' as={Link} to='/home'>
+                          Popular
+                      </Menu.Item>
+                      <Menu.Item>
+                          <Input icon='search' placeholder='Search Movie by title...' />
+                      </Menu.Item>
+                  </Menu>
+              </Grid.Column>
+
+              <Grid.Column width={8}>
+                  <Switch>
+                      <Route exact path='/' component={HomeContainer}/>
+                      <Route exact path='/home' component={HomeContainer}/>
+                      <Route path='/roster' component={HomeContainer}/>
+                      <Route path='/schedule' component={HomeContainer}/>
+                      <Route component={NotFound} />
+                  </Switch>
+              </Grid.Column>
+          </Grid>
       </div>
     );
   }
